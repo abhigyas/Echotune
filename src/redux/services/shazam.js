e2d1b736b2965ed07c0a7d65e34f71bdf1fb1e6d
Abhigya Sinha <sinha.abhigya0721@gmail.com>
2024-01-25 15:24:45 -0500

    throttle
    Throttle component on api in attempts to fix the too many requests error

--- src/redux/services/shazam.js
+++ src/redux/services/shazam.js
@@ -1,22 +1,24 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
-    
-export const shazamApi = createApi({
-    reducerPath: 'shazamApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam-api6.p.rapidapi.com/shazam/',
-        prepareHeaders:(headers) => {
-            headers.set('X-RapidAPI-Key', '861e75ac51msh136447bf360b4e6p1e0e8ejsn66698c226405');
-            
-            return headers;
-        },
-    }),
-        endpoints:(builder) =>({
-           getTopCharts: builder.query({query: () => '/top_tracks_country?country_code=US&limit=50'}),
-           getSongDetails: builder.query ({query: (songid) => `/about_track?track_id=${songid}`}),
-        })
-    });     
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { throttle } from 'lodash';
 
-    export const{
-        useGetTopChartsQuery,
-        useGetSongDetailsQuery,
-    } = shazamApi;
+const throttledFetchBaseQuery = throttle(fetchBaseQuery, 1000); // Adjust the throttle time as needed
+
+export const shazamApi = createApi({
+  reducerPath: 'shazamApi',
+  baseQuery: throttledFetchBaseQuery({
+    baseUrl: 'https://shazam-api6.p.rapidapi.com/shazam/',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '861e75ac51msh136447bf360b4e6p1e0e8ejsn66698c226405');
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/top_tracks_country?country_code=US&limit=50' }),
+    getSongDetails: builder.query({ query: (songid) => `/about_track?track_id=${songid}` }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+  useGetSongDetailsQuery,
+} = shazamApi;
